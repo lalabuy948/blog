a704c14475b939731809c53661bc8ebf15e7af0b
Daniil Popov <lalabuy9948@gmail.com>
2022-10-31 14:11:38 +0100

    .

---
+++ docs/po/sw.js
@@ -0,0 +1,45 @@
+importScripts('workbox-sw.js');
+if (workbox) {
+    console.log("Yay! Workbox is loaded !");
+    workbox.precaching.precacheAndRoute([]);
+
+    workbox.routing.registerRoute(
+        /(.*)others(.*)\.(?:png|gif|jpg)/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                })
+            ]
+        })
+    );
+
+    workbox.routing.registerRoute(
+    // cache js, css, scc files
+        /.*\.(?:css|js|scss|)/,
+        // use cache but update in the background ASAP
+        new workbox.strategies.StaleWhileRevalidate({
+            // use a custom cache name
+            cacheName: "assets",
+        })
+    );
+
+    workbox.routing.registerRoute(
+        new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+        new workbox.strategies.CacheFirst({
+            cacheName: "google-fonts",
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+            ],
+        })
+    );
+
+workbox.core.skipWaiting();
+    workbox.core.clientsClaim();
+} else {
+    console.log("Oops! Workbox didn't load");
+}
